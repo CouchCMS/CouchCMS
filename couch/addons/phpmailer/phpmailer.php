68001d4886ff1001c0ef5c674bece2af7d132724
kksidd <kksidd@gmail.com>
2018-03-04 16:44:35 +0530

    Fix disclosure of site path

--- couch/addons/phpmailer/phpmailer.php
+++ couch/addons/phpmailer/phpmailer.php
@@ -1,4 +1,6 @@
 <?php
+    if ( !defined('K_COUCH_DIR') ) die(); // cannot be loaded directly
+
     class KPHPMailer{
 
         var $config = array();
--- couch/includes/mysql2i/mysql2i.func.php
+++ couch/includes/mysql2i/mysql2i.func.php
@@ -3,6 +3,7 @@
 mysql2i.func.php rev 1
 member of mysql2i.class.php ver 1.0
 */
+if ( defined('K_COUCH_DIR') ){
 function mysql_affected_rows($link=null){
 
     return mysql2i::mysql_affected_rows($link);
@@ -288,5 +289,6 @@ function mysql_unbuffered_query($query,$link=null){
 
     return mysql2i::mysql_unbuffered_query($query,$link);
 
+}
 }
 ?>
