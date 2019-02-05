566ee64ca6d62da8accee0c62eb0e42834d8288d
Anton Smirnov <tony.smirnov@gmail.com>
2019-02-06 00:48:03 +0300

    Let Mosaic respect datetime field's "only_time" param

--- couch/addons/mosaic/theme/fields/ctx-setters.php
+++ couch/addons/mosaic/theme/fields/ctx-setters.php
@@ -46,7 +46,21 @@
                             $formatted_date .= "@$h:$m";
                         }
                     }
+                    
+                    if( $f->only_time ){
+                        $h = substr( $date, 11, 2 );
+                        $m = substr( $date, 14, 2 );
 
+                        if( $f->am_pm ){
+                            // 24-hour time to 12-hour time
+                            list( $h, $m, $a ) = explode( ":", @date("h:i:A", strtotime("$h:$m")) );
+                            $formatted_date = "$h:$m $a";
+                        }
+                        else{
+                            $formatted_date = "$h:$m";
+                        }
+                    }
+                    
                     $CTX->set( 'k_date_formatted', $formatted_date );
                 }
             }
