9a929c94492121485f0c2d22d97a56d67cbb8681
kksidd <kksidd@gmail.com>
2019-04-14 02:55:31 +0530

    Add more event handlers to sanitize

--- couch/parser/HTMLParser.php
+++ couch/parser/HTMLParser.php
@@ -181,6 +181,7 @@
 
             // invalidate other dangerous words
             // https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
+            // https://gist.github.com/kurobeats/9a613c9ab68914312cbb415134795b45
             $ra2 = array(
                  'fscommand', 'onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate',
                  'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus',
@@ -204,6 +205,8 @@
                  'datafld', 'dataformatas', 'datasrc', 'binding', 'behavior',
                  'onformchange', 'onforminput', 'formaction', 'oninput', 'dirname', 'pattern', 'mhtml:',
                  'onhashchange', 'onmessage', 'onoffline', 'ononline', 'onpagehide', 'onpageshow', 'onpopstate', 'onstorage', 'onundo', 'onredo',
+                 'oninvalid', 'onsearch', 'onwheel', 'oncanplay', 'oncuechange', 'ondurationchange', 'onemptied', 'onplay', 'onratechange',
+                 'onstalled', 'onsuspend', 'ontimeupdate', 'onvolumechange', 'onwaiting', 'onshow', 'ontoggle',
                  );
 
             for( $i = 0; $i < count($ra2); $i++ ){
